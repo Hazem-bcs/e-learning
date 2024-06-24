f6516d1c8b6457cc9c528f62c94663cf3ece40cc
Hazem <hazemalfayyad@gmail.com>
2024-06-24 20:30:48 +0300

    new Repo

---
+++ lib/data/data_source/statics/images.dart
@@ -0,0 +1,7 @@
+class AppImageAsset {
+  static const String onBoardingImageOne = "assets/images/onBoardingImageOne";
+  static const String onBoardingImageTow = "assets/images";
+  static const String onBoardingImageThree = "assets/images";
+  static const String onBoardingImageFour = "assets/images";
+  static const String onBoardingImageFive = "assets/images";
+}
